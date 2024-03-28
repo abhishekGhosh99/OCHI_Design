c7439cf1dc79deb52850dd0014b036796db79626
abhishekGhosh99 <125865872+abhishekGhosh99@users.noreply.github.com>
2024-03-29 00:42:09 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/OCHI_Design/",
+  base: "/OCHI_Design_Clone/",
   plugins: [react()],
 });
